5dba44345ff21c8648decd09652a9b42b096b658
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-02-20 11:41:46 -0500

    Add basic calculator functions Created add, substract, multiply, divide functions. Created operate function that takes an operator and calls the above functions. Created buttons for numbers and operators in html.

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function multiply(a,b){
 function divide(a,b){
     return a/b
 }
+
+function operate(firstNumber,operator,secondNumber){
+ switch (operator){
+    case "+" : add(firstNumber,secondNumber)   
+    case "-" : substract(firstNumber,secondNumber)   
+    case "*" : multiply(firstNumber,secondNumber)
+    case "/" : divide(firstNumber,secondNumber)
+    }
+}
